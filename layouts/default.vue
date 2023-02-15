30df96f8141ac62e29f7499588bc859165d81d03
Allan Condiman <rjaycondiman81@gmail.com>
2023-02-16 05:28:25 +0800

    Moving Forward folder layouts

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="wrapper">
+		<!-- Navbar -->
+		<LayoutNavbar />
+		<!-- /.navbar -->
+
+		<!-- Main Sidebar Container -->
+		<LayoutSidebar />
+		<!-- Content Wrapper. Contains page content -->
+		<div class="content-wrapper">
+			<Nuxt/>
+		</div>
+
+		<!-- Main Footer -->
+		<!-- <LayoutFooter /> -->
+		<!--<LayoutFooter />-->
+	</div>
+</template>
+
+<script>
+	export default {
+		head() {
+			return {
+				bodyAttrs: {
+					class: "hold-transition layout-fixed layout-navbar-fixed",
+				},
+			};
+		},
+	};
+</script>
